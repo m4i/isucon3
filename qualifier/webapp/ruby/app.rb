889bd677a4ca6c5b5d574e96881cfe3c54b29a21
Masaki Takeuchi <m.ishihara@gmail.com>
2013-11-08 23:33:35 +0900

    利用されていない users.last_access を削除

--- qualifier/webapp/ruby/app.rb
+++ qualifier/webapp/ruby/app.rb
@@ -136,7 +136,6 @@ class Isucon3App < Sinatra::Base
       session.clear
       session["user_id"] = user["id"]
       session["token"] = Digest::SHA256.hexdigest(Random.new.rand.to_s)
-      mysql.xquery("UPDATE users SET last_access=now() WHERE id=?", user["id"])
       redirect "/mypage"
     else
       erb :signin, :layout => :base, :locals => {
