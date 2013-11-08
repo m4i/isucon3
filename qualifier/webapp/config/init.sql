f8ee08ea5338127bc072e1e8fb148b272c0b7e2e
Masaki Takeuchi <m.ishihara@gmail.com>
2013-11-09 04:56:04 +0900

    INDEX を追加

--- qualifier/webapp/config/init.sql
+++ qualifier/webapp/config/init.sql
@@ -1,2 +1,6 @@
 ALTER TABLE memos ADD header varchar(20000);
 UPDATE memos SET header = SUBSTRING_INDEX(content, '\n', 1);
+
+CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`, `created_at`);
+CREATE INDEX `memos_idx_user_created_at` ON memos (`user`, `created_at`);
+CREATE INDEX `memos_idx_user_is_private_created_at` ON memos (`user`, `is_private`, `created_at`);
